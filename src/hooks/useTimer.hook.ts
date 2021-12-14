ca0511f8e90a45fd05ed198ac143d7f3540fbf38
xTCry [Vladislav Kh] <wolt6@mail.ru>
2021-12-14 23:01:55 +0300

    fix(hooks): fixed `react-hooks/exhaustive-deps`

--- src/hooks/useTimer.hook.ts
+++ src/hooks/useTimer.hook.ts
@@ -7,39 +7,34 @@ const useTimer = (initialState = 0) => {
   const countRef = React.useRef(null);
 
   const handlePause = React.useCallback(() => {
-    console.log('handlePause', { isActive, isPaused });
     if (countRef.current) clearInterval(countRef.current);
     setIsPaused(null);
-  }, [isActive, isPaused, countRef, setIsPaused]);
+  }, [countRef, setIsPaused]);
 
   const handleReset = React.useCallback(() => {
-    console.log('handleReset', { isActive, isPaused });
     handlePause();
     setIsActive(false);
     setTimer(0);
-  }, [isActive, isPaused, setIsActive, setTimer, handlePause]);
+  }, [setIsActive, setTimer, handlePause]);
 
   const handleResume = React.useCallback(() => {
-    console.log('handleResume', { isActive, isPaused });
     if (countRef.current) clearInterval(countRef.current);
     setIsPaused(true);
     countRef.current = setInterval(() => {
       setTimer((timer) => timer + 1);
     }, 1e3);
-  }, [isActive, isPaused, countRef, setIsPaused]);
+  }, [countRef, setIsPaused]);
 
   const handleStartPause = React.useCallback(
     (play = false) => {
       setIsActive(true);
-      console.log('handleStartPause', { isActive, isPaused });
-
       if (!isPaused || play) {
         handleResume();
       } else {
         handlePause();
       }
     },
-    [isActive, isPaused, setIsActive, handleResume, handlePause],
+    [isPaused, setIsActive, handleResume, handlePause],
   );
 
   return { timer, isActive, isPaused, handleStartPause, handlePause, handleResume, handleReset };
